b1841a19ca3d5a567b7f50f77fb47cc45bbdfca5
oskar-snowflake <oskar.eriksson@snowflake.com>
2022-12-06 16:02:03 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+# Import required libraries
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import avg, sum, col,lit
+import streamlit as st
+import pandas as pd
+
+st.text(pd.__version__)
+
